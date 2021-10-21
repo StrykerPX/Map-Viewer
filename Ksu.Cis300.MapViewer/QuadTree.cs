61dc3bdd4fa8984aa63c5e537ea17ac692b3d5ce
Sicheng Chen <sicheng@ksu.edu>
2021-10-21 12:19:05 -0500

    New edit.

--- Ksu.Cis300.MapViewer/QuadTree.cs
+++ Ksu.Cis300.MapViewer/QuadTree.cs
@@ -5,7 +5,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System;
-
+using System.Windows.Forms;
+using System.IO;
 
 namespace Ksu.Cis300.MapViewer
 {
@@ -39,28 +40,21 @@ namespace Ksu.Cis300.MapViewer
             {
                 if (isQuadTreeNode)
                 {
-                   return new BinaryTreeNode<MapData>(mapData, BuildTree(bounds, zoom, !isQuadTreeNode), BuildTree(bounds, zoom, !isQuadTreeNode));
+                    RectangleF leftBounds = new RectangleF(bounds.X, bounds.Y, (bounds.Width) / 2, bounds.Height);
+                    RectangleF rightBounds = new RectangleF(bounds.X + (bounds.Width) / 2, bounds.Y, (bounds.Width) / 2, bounds.Height);
+                    return new BinaryTreeNode<MapData>(mapData, BuildTree(leftBounds, zoom, isQuadTreeNode), BuildTree(rightBounds, zoom, isQuadTreeNode));
                 }
                 else
                 {
-                    if (bounds.Height * bounds.Width < DrawingQuadThreshold)
-                    {
-                        RectangleF leftBounds = new RectangleF(bounds.X, bounds.Y, (bounds.Width) / 2, bounds.Height);
-                        RectangleF rightBounds = new RectangleF((bounds.Width) / 2, bounds.Y, (bounds.Width) / 2, bounds.Height);
-                        return new BinaryTreeNode<MapData>(mapData, BuildTree(leftBounds, zoom, isQuadTreeNode), BuildTree(rightBounds, zoom, isQuadTreeNode));
-                    }
-                    else
-                    {
-                        RectangleF topBounds = new RectangleF(bounds.X, bounds.Y, (bounds.Width) / 2, bounds.Height);
-                        RectangleF bottomBounds = new RectangleF(bounds.X, (bounds.Height) / 2, bounds.Width, (bounds.Height) / 2);
+                    RectangleF topBounds = new RectangleF(bounds.X, bounds.Y, (bounds.Width) / 2, bounds.Height);
+                    RectangleF bottomBounds = new RectangleF(bounds.X, bounds.Y + (bounds.Height) / 2, bounds.Width, (bounds.Height) / 2);
 
-                        if (bounds.Width * bounds.Height < DrawingQuadThreshold)
-                        {
-                            zoom = zoom + 1;
-                        }
-
-                        return new BinaryTreeNode<MapData>(mapData, BuildTree(topBounds, zoom, isQuadTreeNode), BuildTree(bottomBounds, zoom, isQuadTreeNode));
+                    if (bounds.Width * bounds.Height < DrawingQuadThreshold)
+                    {
+                        zoom = zoom + 1;
                     }
+
+                    return new BinaryTreeNode<MapData>(mapData, BuildTree(topBounds, zoom, isQuadTreeNode), BuildTree(bottomBounds, zoom, isQuadTreeNode));
                 }
             }
         }
@@ -79,19 +73,32 @@ namespace Ksu.Cis300.MapViewer
             }
             else
             {
-                float temp;
+                float divideLine;
                 if (t.Data.Lines != null)
                 {
-                    temp = t.Data.Bounds.X + (t.Data.Bounds.Width) / 2;
-                }
-                else if (t.Data.Lines == null)
-                {
-                    temp = t.Data.Bounds.Y + (t.Data.Bounds.Height) / 2;
-                    t.RightChild.Data.Lines.Add(line.Reflect());
+                    divideLine = t.Data.Bounds.X + (t.Data.Bounds.Width) / 2;
                 }
                 else
                 {
+                    divideLine = t.Data.Bounds.Y + (t.Data.Bounds.Height) / 2;
                     
+                }
+
+                if (line.End.X <= divideLine)
+                {
+                    QuadTree.AddLine(t.LeftChild, line.Reflect(), zoom);
+                }else if (line.Start.X >= divideLine)
+                {
+                    QuadTree.AddLine(t.RightChild, line.Reflect(), zoom);
+                }
+                else
+                {
+                    LineSegment left;
+                    LineSegment right;
+
+                    line.Split(divideLine, out left, out right);
+                    QuadTree.AddLine(t.LeftChild, left.Reflect(), zoom);
+                    QuadTree.AddLine(t.RightChild, right.Reflect(), zoom);
                 }
             }
         }
@@ -103,7 +110,21 @@ namespace Ksu.Cis300.MapViewer
         /// <returns></returns>
         public BinaryTreeNode<MapData> ReadFile(string fileName)
         {
+            float width;
+            float height;
 
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                width = float.Parse(sr.ReadLine().Split(',')[0]);
+                height = float.Parse(sr.ReadLine().Split(',')[1]);
+
+                RectangleF rectangle = new RectangleF(0, 0, width, height);
+                QuadTree.BuildTree(rectangle, 0, true);
+                while (sr.ReadLine() != null)
+                {
+                    
+                }
+            }
         }
 
         /// <summary>
